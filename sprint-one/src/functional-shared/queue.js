6bf2875ce4c42f748cc0100668257ac3a524bb14
sunniemao <mao.sunnie@gmail.com>
2016-12-14 15:19:14 -0800

    Implement functional-shared pattern queue.js

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,33 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var instance = {};
+  var lastKey;
+  instance.sizeLength = 0;
+  instance.enqueue = queueMethods.enqueue;
+  instance.dequeue = queueMethods.dequeue;
+  instance.size = queueMethods.size;
+  return instance;
 };
 
 var queueMethods = {};
+queueMethods.size = function() {
+  return this.sizeLength;
+};
+queueMethods.enqueue = function(value) {
+  this.sizeLength ++;
+  lastKey = this.sizeLength;
+  this[lastKey] = value;
+};
+queueMethods.dequeue = function() {
+  var first = this[1];
+  lastKey = this.sizeLength;
+  for (var i = 1; i < this.sizeLength; i++) {
+    this[i] = this[i + 1];
+  }
+  delete this[lastKey];
+  if (lastKey > 0) {
+    this.sizeLength --;
+  }
+  return first;
+};
 
 
