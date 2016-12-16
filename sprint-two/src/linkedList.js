f493e806cbd38b70c2289139a5729446de509694
sunniemao <mao.sunnie@gmail.com>
2016-12-15 16:38:38 -0800

    Implement MVP for linkedList.js

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,36 +2,48 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  list.nodes = [];
 
   list.addToTail = function(value) {
-    list.tail = new Node(value);
+    if (list.tail === null) {
+      list.tail = new Node(value);
+    } else {
+      list.tail.next = new Node(value);
+      list.tail = list.tail.next;
+    }
+    list.nodes.push(list.tail.value);
     if (list.head === null) {
-      list.head = list.tail;
+      list.head = list.tail; 
     } 
   };
 
   list.removeHead = function() {
     var head = list.head;
-    list.head = list.tail;
-    return head.value;
+    list.head = list.head.next;
+    delete list.nodes[0];
+    return head.value; 
   };
 
   list.contains = function(target) {
-    
+    if (list.nodes.indexOf(target) > -1) {
+      return true;
+    } else {
+      return false;
+    }
   };
-
+  
   return list;
 };
 
 var Node = function(value) {
   var node = {};
-
   node.value = value;
   node.next = null;
-
   return node;
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
- */
+ -> Node, add to tail and remove head should be O(1) while contains is a find 
+ operation with O(n). Together, LinkedList as a whole is O(n)
+*/
