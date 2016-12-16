728b402a565e95759fced6bc8cdd7a7c6a595c01
sunniemao <mao.sunnie@gmail.com>
2016-12-16 11:40:46 -0800

    implement MVP for set.js

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,34 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (!this.contains(item)) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  if (this._storage.indexOf(item) > -1) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 setPrototype.remove = function(item) {
+  if (this.contains(item)) {
+    delete this._storage[this._storage.indexOf(item)];
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ O(1): set
+ O(n): add, contains
+ O(n2): remove
  */
