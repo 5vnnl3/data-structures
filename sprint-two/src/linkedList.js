d3b202295ac6416d2560c7ebd2ea45bb00003ebe
sunniemao <mao.sunnie@gmail.com>
2016-12-15 18:11:42 -0800

    refactored linkedList.js

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,30 +2,26 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
-  list.nodes = [];
 
   list.addToTail = function(value) {
     if (list.tail === null) {
       list.tail = new Node(value);
+      list.head = list.tail;
     } else {
       list.tail.next = new Node(value);
       list.tail = list.tail.next;
     }
-    list.nodes.push(list.tail.value);
-    if (list.head === null) {
-      list.head = list.tail; 
-    } 
   };
 
   list.removeHead = function() {
     var head = list.head;
     list.head = list.head.next;
-    delete list.nodes[0];
     return head.value; 
   };
 
   list.contains = function(target) {
-    if (list.nodes.indexOf(target) > -1) {
+    var json = JSON.stringify(list);
+    if (json.indexOf('"value":' + target + ',') > -1) {
       return true;
     } else {
       return false;
