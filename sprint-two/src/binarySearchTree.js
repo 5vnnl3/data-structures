766ec1b96069f52b61338f15355a96aee4559a40
sunniemao <mao.sunnie@gmail.com>
2016-12-16 21:19:21 -0800

    Implement binarySearchTree.js

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,57 @@
 var BinarySearchTree = function(value) {
+  var bst = Object.create(bstMethods);
+  bst.value = value;
+  bst.left = null;
+  bst.right = null;
+  return bst;
 };
 
+bstMethods = {};
+
+bstMethods.insert = function(value) {
+  if (value < this.value) {
+    if (this.left === null) {
+      this.left = BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
+  } else {
+    if (this.right === null) {
+      this.right = BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
+  }
+};
+
+bstMethods.contains = function(value) {  
+  if (this.value === value) {
+    return true;
+  } else if (this.left && value < this.value) {
+    if (this.left.contains(value)) {
+      return true;
+    } 
+  } else if (this.right && value > this.value) {
+    if (this.right.contains(value)) {
+      return true;
+    } 
+  }
+  return false;
+};
+
+bstMethods.depthFirstLog = function(cb) {
+  cb(this.value);
+  if (this.left) {
+    this.left.depthFirstLog(cb);
+  }
+  if (this.right) {
+    this.right.depthFirstLog(cb);
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
+O(1): BinarySearchTree, Insert
+O(log n): Contains
+O(n): DepthFirstLog
  */
